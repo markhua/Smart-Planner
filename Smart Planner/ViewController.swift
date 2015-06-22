8012fe92cc6f7e32a80ec6579595aa027debd153
markhua <huazhi90@hotmail.com>
2015-06-23 07:18:44 +0800

    Initial Commit

---
+++ Smart Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart Planner
+//
+//  Created by Mark Zhang on 15/6/23.
+//  Copyright (c) 2015年 Mark Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
